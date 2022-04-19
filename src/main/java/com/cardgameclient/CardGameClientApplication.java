099349f35e38b66bfb27ab8fba83c713d28d8641
2390634566@qq.com <2390634566@qq.com>
2022-04-19 21:58:46 +0800

    基本配置

---
+++ src/main/java/com/cardgameclient/CardGameClientApplication.java
@@ -0,0 +1,13 @@
+package com.cardgameclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGameClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardGameClientApplication.class, args);
+    }
+
+}
